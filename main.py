9735f5a3f28d4a18d610767bc7f206037b4518ac
NapOB0t <136348439+NapOB0t@users.noreply.github.com>
2023-10-28 17:17:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Band Name Day 1 - Project
+
+print("Welcome to the Band Name application ")
+print("In order to get the name of your band please provide the following information\n1.Home City Name \n2. PetsName")
+print()
+print()
+input("Press Any key to continue ")
+City = input("What was the name of the city you grew up in ? ")
+PetName = input("What was the name of your first pet ? ")
+print()
+print()
+print("The name of your band will be:  "+City+PetName)
